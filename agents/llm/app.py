49de3afd1bf470b764fd085747797d4446831aa8
Benedict Chan <bencxr@fragnetics.com>
2025-04-18 13:36:38 +0800

    dev.py: debug mode, restart on change
    run in debug mode, watch and hot reload when python files are changed

--- agents/llm/app.py
+++ agents/llm/app.py
@@ -1,14 +1,24 @@
 from kradle import AgentManager
 from llm_agent import create_llm_agent_class
 
-qwen25class = create_llm_agent_class({ "username": "qwen25", "model": "qwen/qwen-2.5-coder-32b-instruct" })
-#geminiclass = create_llm_agent_class({ "username": "gemini", "model": "google/gemini-2.0-flash-001" })
-#gpt4oclass = create_llm_agent_class({ "username": "gpt4o", "model": "openai/gpt-4o-mini" })
+import os
 
-# Create and serve our agent
-# This creates a web server and an SSH tunnel (so our agent has a stable public URL)
-app, connection_info = AgentManager.serve(qwen25class, create_public_url=True)
-#app, connection_info = AgentManager.serve(geminiclass, create_public_url=True)
-#app, connection_info = AgentManager.serve(gpt4oclass, create_public_url=True)
+def app(debug=False):
+    try:
+        # gemini_class = create_llm_agent_class({ "username": "gemini", "model": "google/gemini-2.0-flash-001" })
+        # gpt4o_class = create_llm_agent_class({ "username": "gpt4o", "model": "openai/gpt-4o-mini" })
+        qwen25_class = create_llm_agent_class({ "username": "qwen25", "model": "qwen/qwen-2.5-coder-32b-instruct" })
 
-# now go to app.kradle.ai and run this agent against a challenge!
+        # Create and serve our agent
+        # This creates a web server and an SSH tunnel (so our agent has a stable public URL)
+        # AgentManager.serve(gemini_class, create_public_url=True)
+        # AgentManager.serve(gpt4o_class, create_public_url=True)
+        app, url = AgentManager.serve(qwen25_class, create_public_url=True, debug=debug)
+    except KeyboardInterrupt:
+        os._exit(0)
+
+    # now go to app.kradle.ai and run this agent against a challenge!
+    print(f"Started agent, now reachable at URL: {url}\n", flush=True)
+
+if __name__ == "__main__":
+    app()
